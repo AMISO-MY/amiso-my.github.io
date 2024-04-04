534600cea54753776cd14fa5b6b336ba9954e0a5
ongzzzzzz <ongzzzzzz@users.noreply.github.com>
2024-04-04 14:16:56 +0000

    deploy: 8638d67c9afed15a2aed161ed6bef31a2d88eb6a

--- assets/js/ysec.js
+++ assets/js/ysec.js
@@ -1,3 +1,4 @@
+"use strict";
 
 let prelimDate = new Date("Nov 25, 2023 10:00:00").getTime();
 console.log(prelimDate);
@@ -21,44 +22,3 @@ let countdown = setInterval(() => {
         document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
     }
 }, 1000);
-
-
-
-let cmts = [
-    { name: "Ong Zhi Zheng", pos: "Advisor" },
-    { name: "Joyton Fu Hung Li", pos: "Advisor" },
-    { name: "Chang Kian Yau", pos: "Advisor" },
-    { name: "Liew Ze Wei", pos: "General Management Team" },
-    { name: "Lee Ke Yin", pos: "General Management Team" },
-    { name: "Joel Pang Kai Chen", pos: "General Management Team" },
-    { name: "Ng Ze Xian", pos: "General Management Team" },
-    { name: "Lee Han Yang", pos: "Secretary" },
-    { name: "Tan Zhi Qi", pos: "Secretary" },
-    { name: "Bok Zhe Shing", pos: "Treasurer" },
-    { name: "Quincy Jules Tan Yi Liing", pos: "Publicity" },
-    { name: "Eunice Lee Xin Yi", pos: "Publicity" },
-    { name: "Ong Kok Rhui", pos: "Publicity" },
-    { name: "Lam Wei Heng", pos: "Publicity" },
-    { name: "Wong Jer Ren", pos: "Problem Selection Committee Head" },
-    { name: "Nelson Loh Kwong Weng", pos: "Problem Selection Committee" },
-    { name: "Aaron Oong Zhu Wen", pos: "Problem Selection Committee" },
-    { name: "Ng Kin Him", pos: "Problem Selection Committee" },
-    { name: "Tan Zhi Qi", pos: "Problem Selection Committee" },
-    { name: "Shee Yi Rong", pos: "Logistics" },
-    { name: "Ching Wei Yang", pos: "Logistics" },
-    { name: "Ooi Zhen Khoon", pos: "Logistics" },
-    // { name: "Leong Zheng Haw", pos: "Volunteer" },
-];
-
-window.addEventListener("load", () => {
-    for (let cmt of cmts) {
-        let cmtNode = document.createElement("div");
-        cmtNode.className = "tooltip";
-        cmtNode.innerHTML = `
-        <img src="/assets/images/cmt/${cmt.name.replace(/ /g, "_")}.jpg" alt="Picture of ${cmt.name}/>
-        <h6>${cmt.name}</h6>
-        <span class="tooltiptext">${cmt.pos}</span>
-    `;
-        document.getElementById("cmts").appendChild(cmtNode);
-    }
-})
